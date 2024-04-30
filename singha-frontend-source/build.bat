4ea2217989706f8f2be7124b77f14be47c521e93
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-04-30 12:53:08 +0700

    deploy

--- singha-frontend-source/build.bat
+++ singha-frontend-source/build.bat
@@ -1,9 +1,8 @@
 chcp 65001
 CALL npm run build
 xcopy /s/All C:\xampp\htdocs\singha-frontend\singha-frontend-source\dist C:\xampp\htdocs\singha-frontend
-pause
 cd C:\xampp\htdocs\singha-frontend
-CALL git pull
 CALL git add .
 CALL git commit -m "deploy"
+CALL git pull
 CALL git push

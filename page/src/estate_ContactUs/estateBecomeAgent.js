eb1138dc9f051f250de60df186ebb6a93c5c5b62
HobbyCreate <jathurong.sugruk@mirumagency.com>
2024-06-18 15:24:33 +0700

    fix front-end mobile

--- page/src/estate_ContactUs/estateBecomeAgent.js
+++ page/src/estate_ContactUs/estateBecomeAgent.js
@@ -31,39 +31,39 @@ document.addEventListener('DOMContentLoaded', function () {
             center: true,
             margin: 0,
             responsive: {
-                0: { 
-                    items: 1, 
-                    margin: 10, 
-                    stagePadding: 40, 
+                0: {
+                    items: 1,
+                    margin: 10,
+                    stagePadding: 40,
                 },
-                560: { 
-                    items: 1, 
-                    margin: 0, 
-                    stagePadding: 100 ,
+                560: {
+                    items: 1,
+                    margin: 0,
+                    stagePadding: 60,
                 },
-                768: { 
-                    items: 1.5, 
-                    stagePadding: 60 ,
+                768: {
+                    items: 1.5,
+                    stagePadding: 60,
                 },
-                991: { 
-                    items: 2 , 
-                    stagePadding: 60 ,
+                991: {
+                    items: 2,
+                    stagePadding: 60,
                 },
-                1024: { 
+                1024: {
                     items: 3,
-                    stagePadding: 10 ,
+                    stagePadding: 10,
                 },
-                1366: { 
+                1366: {
                     items: 3.5,
-                    stagePadding: 100 ,
+                    stagePadding: 100,
                 },
-                1720: { 
+                1720: {
                     items: 3.5,
-                    stagePadding: 120 ,
+                    stagePadding: 120,
                 },
-                1920: { 
-                    items: 4.5 ,
-                    stagePadding: 20 ,
+                1920: {
+                    items: 4.5,
+                    stagePadding: 20,
                 },
             },
         });
@@ -91,30 +91,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Handle window resize to reinitialize the portfolio carousel
     let resizeTimeout;
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         clearTimeout(resizeTimeout);
-        resizeTimeout = setTimeout(function() {
+        resizeTimeout = setTimeout(function () {
             $('.portfolio.owl-carousel').owlCarousel('destroy');
             initializePortfolioCarousel();
         }, 50); // Delay to avoid excessive reinitializations during resize
     });
 
     // form phone number dropdown part
-    document.getElementById('PRESET_PHONE').addEventListener('change', function() {
+    document.getElementById('PRESET_PHONE').addEventListener('change', function () {
         var selectedOption = this.options[this.selectedIndex];
         var fullText = selectedOption.getAttribute('data-fulltext');
         selectedOption.text = selectedOption.value;
         selectedOption.setAttribute('data-fulltext', fullText);
     });
-    
-    document.getElementById('PRESET_PHONE').addEventListener('click', function() {
+
+    document.getElementById('PRESET_PHONE').addEventListener('click', function () {
         for (var i = 0; i < this.options.length; i++) {
             var option = this.options[i];
             option.text = option.getAttribute('data-fulltext');
         }
     });
-    
-    document.getElementById('PRESET_PHONE').addEventListener('blur', function() {
+
+    document.getElementById('PRESET_PHONE').addEventListener('blur', function () {
         var selectedOption = this.options[this.selectedIndex];
         selectedOption.text = selectedOption.value;
     }, true);

e0003ccf342bf16ee7416e8e17bcfa4b171c3f87
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-06-19 21:58:20 +0700

    update js

--- component/hero-banner/hero-banner.js
+++ component/hero-banner/hero-banner.js
@@ -10,25 +10,43 @@ var bannerDetailSwipe = new Swiper(".banner-detail-swipe", {
     },
     on: {
         init: function (e) {
-            document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime = 2;
-            document.querySelector("#hero-slide-vdo-" + e.activeIndex).play();
-            document.querySelector("#hero-slide-vdo-" + e.activeIndex).addEventListener("timeupdate", (event) => {
-                // totalLength = document.querySelector("#hero-slide-vdo-" + e.activeIndex).duration % 60;
-                totalLength = 5;
-                percentageCompleted = (document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime + 5 / totalLength) * 100 - 40;
-                document.querySelector('.banner-detail-swipe .custom-pagination-square .swiper-pagination-bullet-active').style.setProperty('--vdo-width', percentageCompleted + "%")
-            });
+
+            var playPromise = document.querySelector("#hero-slide-vdo-" + e.activeIndex).play();
+
+            if (playPromise !== undefined) {
+                playPromise.then(_ => {
+                    document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime = 2;
+                    document.querySelector("#hero-slide-vdo-" + e.activeIndex).addEventListener("timeupdate", (event) => {
+                        // totalLength = document.querySelector("#hero-slide-vdo-" + e.activeIndex).duration % 60;
+                        totalLength = 5;
+                        percentageCompleted = (document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime + 5 / totalLength) * 100 - 40;
+                        document.querySelector('.banner-detail-swipe .custom-pagination-square .swiper-pagination-bullet-active').style.setProperty('--vdo-width', percentageCompleted + "%")
+                    });
+                })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                    });
+            }
         },
         activeIndexChange: function (e) {
-                console.log(e.activeIndex,'aaa');
-            document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime = 2;
-            document.querySelector("#hero-slide-vdo-" + e.activeIndex).play();
-            document.querySelector("#hero-slide-vdo-" + e.activeIndex).addEventListener("timeupdate", (event) => {
-                // totalLength = document.querySelector("#hero-slide-vdo-" + e.activeIndex).duration % 60;
-                totalLength = 5;
-                percentageCompleted = (document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime / totalLength) * 100 - 40;
-                document.querySelector('.banner-detail-swipe .custom-pagination-square .swiper-pagination-bullet-active').style.setProperty('--vdo-width', percentageCompleted + "%")
-            });
+            var playPromise = document.querySelector("#hero-slide-vdo-" + e.activeIndex).play();
+            
+            if (playPromise !== undefined) {
+                playPromise.then(_ => {
+                    document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime = 2;
+                    document.querySelector("#hero-slide-vdo-" + e.activeIndex).addEventListener("timeupdate", (event) => {
+                        // totalLength = document.querySelector("#hero-slide-vdo-" + e.activeIndex).duration % 60;
+                        totalLength = 5;
+                        percentageCompleted = (document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime / totalLength) * 100 - 40;
+                        document.querySelector('.banner-detail-swipe .custom-pagination-square .swiper-pagination-bullet-active').style.setProperty('--vdo-width', percentageCompleted + "%")
+                    });
+                })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                    });
+            }
         }
     },
 });

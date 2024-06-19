37e2101c4a048d2bb01aa062533dbc811275c959
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-06-19 19:07:29 +0700

    update js version

--- component/hero-banner/hero-banner.js
+++ component/hero-banner/hero-banner.js
@@ -10,6 +10,7 @@ var bannerDetailSwipe = new Swiper(".banner-detail-swipe", {
     },
     on: {
         init: function (e) {
+            document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime = 1;
             document.querySelector("#hero-slide-vdo-" + e.activeIndex).play();
             document.querySelector("#hero-slide-vdo-" + e.activeIndex).addEventListener("timeupdate", (event) => {
                 // totalLength = document.querySelector("#hero-slide-vdo-" + e.activeIndex).duration % 60;
@@ -19,7 +20,7 @@ var bannerDetailSwipe = new Swiper(".banner-detail-swipe", {
             });
         },
         slideChange: function (e) {
-            document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime = 0;
+            document.querySelector("#hero-slide-vdo-" + e.activeIndex).currentTime = 1;
             document.querySelector("#hero-slide-vdo-" + e.activeIndex).play();
             document.querySelector("#hero-slide-vdo-" + e.activeIndex).addEventListener("timeupdate", (event) => {
                 // totalLength = document.querySelector("#hero-slide-vdo-" + e.activeIndex).duration % 60;

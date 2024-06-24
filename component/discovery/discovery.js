8f1f70ee1c991822671437a1613d2ce99674c31a
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-06-24 23:06:29 +0700

    fix feedback

--- component/discovery/discovery.js
+++ component/discovery/discovery.js
@@ -17,7 +17,7 @@ function toggleView() {
     document.querySelector('#discovery').setAttribute('attr-list-type', event.target.getAttribute("attr-icon"));
 }
 
-async function setDiscoveryHtml() {
+async function setDiscoveryHtml(ev) {
     const resp = await getDiscovery();
     var cardList = document.querySelector('.card-list');
     cardList.innerHTML = ''
@@ -36,23 +36,25 @@ async function setDiscoveryHtml() {
                         <div>
                             <img src="${data.data.s}" alt="" class="w-full">
                         </div>
-                        <div class=" flex w-full relative -mt-10">
+                        <div class=" flex w-full relative -mt-10 bg-white/50">
                             <div class="bg-[#E9E2DC]/50 absolute top-0 left-0 w-full h-full backdrop-blur-md"></div z-0>
                             <div class="relative">
-                                <img src="${border}" alt="">
+                                <img src="${border}" alt="" class="lg:w-[20px] w-[16px]">
                             </div>
-                            <div class="flex flex-col p-5 w-full relative">
+                            <div class="flex flex-col p-5 lg:py-5 py-2 w-full relative">
                                 <div
                                     class="${data.data.new == true ? 'hidden lg:block' : 'hidden'} lg:text-[16px] text-[12px] bg-[url('./../assets/icon/badge.svg')] w-auto top-0 lg:right-0 lg:mt-5 lg:left-auto left-0 lg:mr-5 absolute capitalize bg-no-repeat bg-cover px-5 py-1 text-white font-bold text-center">
                                     New Project</div>
                                 <div class="text-[20px] uppercase font-bold">${data.data.brands}</div>
                                 <div>${data.data.location}</div>
-                                <div class="mt-3 uppercase text-[#707070] text-[14px]">start ${data.data.price}</div>
+                                <div class="lg:mt-3 uppercase text-[#707070] text-[14px]">start ${data.data.price}</div>
                             </div>
                         </div>
                     </div>`
     }).join('');
-
     discoveryNum += 4;
+    if (discoveryNum >= resp.length) {
+        ev.classList.add('hidden');
+    } 
     cardList.innerHTML = temp;
 }

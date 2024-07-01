ba9c398e31e085361c8f20c6d7cb22ad3931ad04
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-07-01 17:55:15 +0700

    ีupdate footer

--- component/footer/footer.js
+++ component/footer/footer.js
@@ -72,7 +72,7 @@ async function exportFooter() {
                          <div class="flex flex-col lg:w-1/4 w-full gap-5">
                              <div class="w-full">
                                  <div class="flex flex-col gap-5"><img src="./assets/image/residential/logo-footer.svg"
-                                         class="w-[220px] lg:mx-auto">
+                                         class="w-[220px]">
                                      <!-- <p class="uppercase mt-5 md:text-left text-center"><b>singha estate pcl.</b></p> -->
                                      <p class="text-left lg:text-center font-['IBM_Plex_Sans_Thai']">
                                          บริษัท สิงห์ เอสเตท จำกัด (มหาชน)

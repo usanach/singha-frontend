e1617933380372ebcf6c66eb7d293cb5c4518a75
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-08-15 17:28:53 +0700

    fix

--- component/header/header.js
+++ component/header/header.js
@@ -222,19 +222,19 @@ headerTemplate().then(() => {
         scrub: 1,
     });
     ScrollTrigger.create({
-        trigger: ".header-trigger",
-        start: "top bottom",
+        trigger: "body",
+        start: "+=70 top",
         scrub: 1,
         onUpdate: (self) => {
             if (self.progress > 0) {
                 document.querySelector('.header-bg').classList.add('bg-[#1A2F4D]/75');
                 document.querySelector('.header-bg').classList.remove('bg-[#1A2F4D]');
-                document.querySelector('.header-bg .animate-h').classList.add('md:h-[80px]');
+                document.querySelector('.header-bg .animate-h').classList.add('md:h-[70px]');
                 document.querySelector('.header-bg .animate-h').classList.remove('md:h-[60px]');
             } else {
                 document.querySelector('.header-bg').classList.add('bg-[#1A2F4D]');
                 document.querySelector('.header-bg').classList.remove('bg-[#1A2F4D]/75');
-                document.querySelector('.header-bg .animate-h').classList.remove('md:h-[80px]');
+                document.querySelector('.header-bg .animate-h').classList.remove('md:h-[70px]');
                 document.querySelector('.header-bg .animate-h').classList.add('md:h-[60px]');
             }
         }

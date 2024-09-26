5a17e5a1453f54f977ccb40a51ae7246ff6946f9
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-09-27 02:43:50 +0700

    fix image and popup

--- page/contact/main.js
+++ page/contact/main.js
@@ -14,57 +14,63 @@ createApp({
 
         return {
             font: getLanguageFromPath() == 'en' ? "font-['Cinzel']" : "!font-['IBMPlexSansThai']",
-            data: getLanguageFromPath() == 'en' ? [
+            lang: getLanguageFromPath(),
+            data: [
                 {
-                    title: "CONTACT US",
-                    detail: "Interested in our residential projects",
-                    more: "Explore more",
+                    title: {
+                        en: "CONTACT US",
+                        th: "ติดต่อเรา"
+                    },
+                    detail: {
+                        en: "Interested in our residential projects",
+                        th: "สนใจรายละเอียดโครงการที่พักอาศัย"
+                    },
+                    link: "/" + getLanguageFromPath() + "/contact-us/contact",
+                    target:"_self",
+                    more: {
+                        en: "Explore more",
+                        th: "ข้อมูลเพิ่มเติม​"
+                    },
                     image: {
                         s: "/assets/image/ContactUs/1.png",
                         l: "/assets/image/ContactUs/1-1.png"
                     }
                 },
                 {
-                    title: "BECOME AGENT",
-                    detail: "Project information and partnership terms ",
-                    more: "Explore more",
+                    title: {
+                        en: "BECOME AGENT",
+                        th: "สมัครเป็นตัวแทนขาย​"
+                    },
+                    detail: {
+                        en: "Project information and partnership terms ",
+                        th: "ข้อมูลโครงการและเงื่อนไขผลตอบแทน"
+                    },
+                    link: "/" + getLanguageFromPath() + "/contact-us/become-agent",
+                    target:"_self",
+                    more: {
+                        en: "Explore more",
+                        th: "ข้อมูลเพิ่มเติม​"
+                    },
                     image: {
                         s: "/assets/image/ContactUs/2.png",
                         l: "/assets/image/ContactUs/1-2.png"
                     }
                 },
                 {
-                    title: "Explore more​",
-                    detail: "Types of land / buildings for sale",
-                    more: "Explore more",
-                    image: {
-                        s: "/assets/image/ContactUs/3.png",
-                        l: "/assets/image/ContactUs/1-3.png"
-                    }
-                },
-            ] : [
-                {
-                    title: "ติดต่อเรา",
-                    detail: "สนใจรายละเอียดโครงการที่พักอาศัย",
-                    more: "ข้อมูลเพิ่มเติม​",
-                    image: {
-                        s: "/assets/image/ContactUs/1.png",
-                        l: "/assets/image/ContactUs/1-1.png"
-                    }
-                },
-                {
-                    title: "สมัครเป็นตัวแทนขาย​",
-                    detail: "ข้อมูลโครงการและเงื่อนไขผลตอบแทน ",
-                    more: "ข้อมูลเพิ่มเติม​",
-                    image: {
-                        s: "/assets/image/ContactUs/2.png",
-                        l: "/assets/image/ContactUs/1-2.png"
-                    }
-                },
-                {
-                    title: "เสนอขายที่ดิน/อาคาร​",
-                    detail: "ประเภทที่ดิน/อาคารที่ต้องการขาย​",
-                    more: "ข้อมูลเพิ่มเติม",
+                    title: {
+                        en: "Explore more​",
+                        th: "เสนอขายที่ดิน/อาคาร​",
+                    },
+                    detail: {
+                        en: "Types of land / buildings for sale",
+                        th: "ประเภทที่ดิน/อาคารที่ต้องการขาย​"
+                    },
+                    target:"_blank",
+                    link: "https://property.singhaestate.co.th/en/property-offer?_ga=2.266121743.1474642135.1727241573-885017993.1707963665&_gac=1.224594792.1724900635.CjwKCAjwlbu2BhA3EiwA3yXyu0JpthQzF-0t4GTZ_zA71-2X9vs-f0zjNgSBLTJRVo-X3dqnTudJ7BoCuH4QAvD_BwE#overview",
+                    more: {
+                        en: "Explore more",
+                        th: "ข้อมูลเพิ่มเติม​"
+                    },
                     image: {
                         s: "/assets/image/ContactUs/3.png",
                         l: "/assets/image/ContactUs/1-3.png"
--- page/contactUs/main.js
+++ page/contactUs/main.js
@@ -17,10 +17,14 @@ createApp({
             title: getLanguageFromPath() == 'en' ? "CONTACT US " : "ติดต่อเรา",
             location: {
                 title: getLanguageFromPath() == 'en' ? "OUR LOCATION" : "ที่ตั้ง",
-                direction:getLanguageFromPath() == 'en' ? "Get Direction" : "การเดินทาง",
+                direction: getLanguageFromPath() == 'en' ? "Get Direction" : "การเดินทาง",
+                detail: getLanguageFromPath() == 'en'
+                    ? "SINGHA ESTATE PCL. 123 Suntowers Building B, 40th Floor, Vibhavadi-Rangsit Road, Chom Phon, Chatuchak, Bangkok 10900"
+                    : "บริษัท สิงห์ เอสเตท จำกัด (มหาชน) อาคารซันทาวเวอร์ส บี, ชั้น 40 เลขที่ 123 ถนนวิภาวดีรังสิต แขวงจอมพล เขตจตุจักร​ กรุงเทพมหานคร 10900"
+
             },
             email: {
-                title: getLanguageFromPath() == 'en' ? "EMAIL US " : "อีเมล์",
+                title: getLanguageFromPath() == 'en' ? "EMAIL US " : "อีเมล",
             },
             call: {
                 title: getLanguageFromPath() == 'en' ? "CALL US" : "ติดต่อ",

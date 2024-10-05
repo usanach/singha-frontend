10fefc4e88c07c4b995545d5cb8b2263a594a3bf
Fast <107022521+tanawatfast@users.noreply.github.com>
2024-10-05 16:24:59 +0700

    fix

--- page/home/component/learnMore/component.js
+++ page/home/component/learnMore/component.js
@@ -11,7 +11,7 @@ const LearnMoreComponent = defineComponent({
         const getLanguageFromPath = () => {
             const path = window.location.pathname;
             const match = path.match(/\/(th|en)(\/|$)/);
-            return match ? match[1] : 'en'; // Default to 'th' if not found
+            return match ? match[1] : 'th'; // Default to 'th' if not found
         };
 
         const loadTemplate = async (lang) => {

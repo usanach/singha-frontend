3d84f93728c29ccd90413d046d2e81d23bc96219
HobbyCreate <jathurong.sugruk@mirumagency.com>
2024-12-23 10:16:03 +0700

    - infinity living section - update output.css

--- page/srin/srin-content-page/prannok/component/craft-your-tale/component.js
+++ page/srin/srin-content-page/prannok/component/craft-your-tale/component.js
@@ -2,20 +2,21 @@
 const CraftYourTaleComponent = defineComponent({
     name: 'CraftYourTaleComponent',
     template: `
-    <section class="craft-your-tale-component relative overflow-hidden lg:max-h-[650px] max-h-[600px]" data-section="property_introduction">
+    <section class="craft-your-tale-component relative overflow-hidden lg:max-h-[1020px] max-h-[750px]" data-section="property_introduction">
         <div class="w-full overflow-hidden cyt-desktop-pin">
             <div id="layout-2" class="layout-2 absolute inset-0 flex items-center justify-center w-full h-screen cty-pallax -top-[10rem]"  data-cty-pallax-speed="-3">
                 <div class="w-full h-full">
-                    <img src="/assets/image/page-smyth-kaset/craft-yours-tale/img.png" alt="" class="w-full lg:block hidden">
-                    <img src="/assets/image/page-smyth-kaset/craft-yours-tale/img-m.png" alt="" class="w-full lg:hidden block h-full">
+                    <img src="/assets/image/page-srin-prannok/infinity-living/img.png" alt="" class="w-full lg:block hidden">
+                    <img src="/assets/image/page-srin-prannok/infinity-living/img-m.png" alt="" class="w-full lg:hidden block h-full">
                     <div class="absolute top-0 left-0 h-full w-full flex">
                         <div class="flex flex-col m-auto">
-                            <div>
-                                <img src="/assets/image/page-smyth-kaset/craft-yours-tale/smyth_logo.png" alt="" class="max-w-[300px]" data-aos="fade-up" data-aos-duration="500" data-aos-easing="linear">
-                            </div>
                             <div class="mt-3">  
-                                <p class="font-['Tenor_Sans'] text-white text-[30px] text-center cyt-desc" data-aos="fade-up" data-aos-duration="500" data-aos-easing="linear" data-aos-delay="500">
+                                <p class="font-['The Seasons'] text-white text-[40px] text-center cyt-desc" data-aos="fade-up" data-aos-duration="500" data-aos-easing="linear" data-aos-delay="500">
                                     INFINITE LIVING
+                                </p>
+                                <br>
+                                <p  class="font-['DB OnUma'] text-white text-[30px] text-center cyt-desc" data-aos="fade-up" data-aos-duration="500" data-aos-easing="linear" data-aos-delay="500">
+                                    บ้านเพื่อทุกการเติบโตไม่มีที่สิ้นสุด
                                 </p>
                             </div>
                         </div>
@@ -25,8 +26,8 @@ const CraftYourTaleComponent = defineComponent({
 
             <!-- Layout 1 (Foreground Layer) -->
             <div id="layout-1" class="layout-1 relative inset-0 flex items-center justify-center w-full transition-all duration-1000 h-screen">
-                <img src="/assets/image/page-smyth-kaset/craft-yours-tale/sm.png" alt="" class="w-full lg:block hidden h-full">
-                <img src="/assets/image/page-smyth-kaset/craft-yours-tale/sm-m.png" alt="" class="w-full lg:hidden block h-full">
+                <img src="/assets/image/page-srin-prannok/infinity-living/sm.png" alt="" class="w-full lg:block hidden h-full">
+                <img src="/assets/image/page-srin-prannok/infinity-living/sm-m.png" alt="" class="w-full lg:hidden block h-full">
             </div>
         </div>
     </section>`,

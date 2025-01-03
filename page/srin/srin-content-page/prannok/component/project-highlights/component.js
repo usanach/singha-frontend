240c0f76c46d54a222e8fbf189bcf805ef850f68
HobbyCreate <jathurong.sugruk@mirumagency.com>
2025-01-03 10:46:09 +0700

    - fix font size mobile

--- page/srin/srin-content-page/prannok/component/project-highlights/component.js
+++ page/srin/srin-content-page/prannok/component/project-highlights/component.js
@@ -8,7 +8,7 @@ const ProjectsHighlightComponent = defineComponent({
                 <div class="container mx-auto lg:px-5 px-0">
                     <div class="flex flex-col gap-10">
                         <div>
-                            <h2 class="font-['DB_OnUma'] text-[#013B5E] text-[70px] text-center" data-aos="fade-up"
+                            <h2 class="font-['DB_OnUma'] text-[#013B5E] text-[50px] lg:text-[70px] text-center" data-aos="fade-up"
                                 data-aos-duration="500" data-aos-easing="linear">
                                 {{title['th']}}
                             </h2>

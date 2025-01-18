a07ac1dd552854e178fa6db06bfc2138ca5bbd2a
HobbyCreate <jathurong.sugruk@mirumagency.com>
2025-01-19 04:39:23 +0700

    -test 1

--- page/story/component/banner/component.js
+++ page/story/component/banner/component.js
@@ -28,7 +28,7 @@ const BannerComponent = defineComponent({
                 }
                 const templateResponse = await axios.get('/page/story/component/banner/template.html');
                 let templateContent = templateResponse.data;
-                let specificIndex = [5,7,5]
+                let specificIndex = [5,7,4]
                 // Replace placeholders with actual data
                 templateContent = templateContent
                     .replace(/{{language}}/g, lang)

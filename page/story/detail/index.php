6d8f921573a54b92c220c3df62dbf204b0c9d787
HobbyCreate <jathurong.sugruk@mirumagency.com>
2025-01-21 12:43:02 +0700

    -fix og image shared

--- page/story/detail/index.php
+++ page/story/detail/index.php
@@ -80,7 +80,7 @@
                     'th' => 'S\'RIN ราชพฤกษ์-สาย 1 โครงการบ้านเดี่ยวระดับลักซ์ชัวรี สไตล์ Modern Tropical ที่ผสานความงามของสถาปัตยกรรมเข้ากับธรรมชาติ บนแนวคิด Crafted to Last พร้อมฟังก์ชันครบครัน ตอบโจทย์คนทุกรุ่นในครอบครัว'
                 ],
                 'topic' => 'S\'RIN ราชพฤกษ์-สาย 1, S’RIN Ratchapruek, บ้าน เดี่ยว ราชพฤกษ์',
-                's' => '/assets/image/content/content11/images/H1/og_m.jpg',
+                's' => '/assets/image/content/content11/images/H1/og_m.webp',
                 'url' => [
                     'en' => "/en/stories/sblog/review-srin-ratchaphruek-sai1",
                     'th' =>  "/th/stories/sblog/review-srin-ratchaphruek-sai1"
@@ -98,7 +98,7 @@
                     'th' => 'SHAWN Panya Indra (ฌอน ปัญญาอินทรา) บ้านเดี่ยวระดับลักซ์ชัวรี บนดีไซน์ Modern Tropical ที่สะท้อนตัวตนผ่านฟังก์ชันที่ครบครันสำหรับครอบครัว Multi-Generation บนทำเลศักยภาพรามอินทรา'
                 ],
                 'topic' => 'shawn panya indra, shawn ปัญญาอินทรา, ฌอน ปัญญาอินทรา, บ้าน รามอินทรา',
-                's' => '/assets/image/content/content12/images/H1/og_m.jpg',
+                's' => '/assets/image/content/content12/images/H1/og_m.webp',
                 'url' => [
                     'en' => "/en/stories/sblog/review-shawnpanyaindra",
                     'th' =>  "/th/stories/sblog/review-shawnpanyaindra"

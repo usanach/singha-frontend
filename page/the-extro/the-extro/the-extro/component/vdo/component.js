70d7f8324eb908eff87833b14c35f092553fe280
Fast <tanawat.wongsawan@vml.com>
2025-04-30 11:04:08 +0700

    fix

--- page/the-extro/the-extro/the-extro/component/vdo/component.js
+++ page/the-extro/the-extro/the-extro/component/vdo/component.js
@@ -12,7 +12,7 @@ const VdoComponent = defineComponent({
                 <!-- Title Section -->
                 <div class="flex flex-col justify-center gap-5">
                   <div>
-                    <h2 class="text-white lg:text-[40px] text-[24px] text-center uppercase" :class="[fontCss()]">{{texts.title[language]}}</h2>
+                    <h2 class="text-[#3D2120] lg:text-[40px] text-[24px] text-center uppercase" :class="[fontCss()]">{{texts.title[language]}}</h2>
                   </div>
                   <!-- Image/Video Section -->
                   <div class="mx-auto overflow-hidden relative lg:w-[960px] lg:h-[540px] md:h-[420px] md:w-[730px]">

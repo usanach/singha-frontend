5d084d601bdf074853a8ab347163397796f034fa
Fast <tanawat.wongsawan@vml.com>
2025-05-13 12:10:19 +0700

    fix live extra

--- page/the-extro/the-extro/the-extro/component/craft-your-tale/component.js
+++ page/the-extro/the-extro/the-extro/component/craft-your-tale/component.js
@@ -8,7 +8,7 @@ const CraftYourTaleComponent = defineComponent({
         </div>
         
         <div class="absolute bottom-10  -translate-x-[17%]">
-          <img src="/assets/image/page-the-extro/the-extro/Live-EXTRA.png" class="live-extra" alt="Live Extra"/>
+          <img src="/assets/image/page-the-extro/the-extro/Live-EXTRA.png" class="live-extra2" alt="Live Extra"/>
         </div>
         <div class="m-auto lg:w-3/5 w-full md:h-[500px] h-[400px] relative">
           <img class="w-full h-full object-cover panorama" src="/assets/image/page-the-extro/the-extro/_VPX5685 Panorama.png" alt="Tale Image"/>
@@ -18,30 +18,31 @@ const CraftYourTaleComponent = defineComponent({
   `,
   setup() {
     const language = ref('th'); // Default language
-  
+
     // Function to extract language from the URL
     const getLanguageFromPath = () => {
       const path = window.location.pathname;
       const match = path.match(/\/(th|en)(\/|$)/);
       return match ? match[1] : 'th';
     };
-  
+
     onMounted(async () => {
       language.value = getLanguageFromPath();
       gsap.registerPlugin(ScrollTrigger);
-  
+
       const init = () => {
         AOS.init();
       };
-  
+
       nextTick(() => {
         init();
-  
+
         // GSAP animation for the "Live EXTRA" image
         gsap.fromTo(
           ".live-extra",
-          { y: 100 }, // Start 100px lower than its natural position
-          { y: 0,
+          { y: window.innerWidth > 768 ? 100 : 200 }, // Start 100px lower than its natural position
+          {
+            y: 0,
             ease: "none",
             scrollTrigger: {
               trigger: ".live-extra",
@@ -51,7 +52,21 @@ const CraftYourTaleComponent = defineComponent({
             },
           }
         );
-  
+        gsap.fromTo(
+          ".live-extra2",
+          { y: 100}, // Start 100px lower than its natural position
+          {
+            y: 0,
+            ease: "none",
+            scrollTrigger: {
+              trigger: ".live-extra",
+              start: "top bottom",
+              end: "bottom top",
+              scrub: true,
+            },
+          }
+        );
+
         // GSAP animation for the panorama image to create a parallax effect
         // gsap.fromTo(
         //   ".panorama",
@@ -66,12 +81,13 @@ const CraftYourTaleComponent = defineComponent({
         //     },
         //   }
         // );
-  
+
         // Additional animation: Fade in the whole section as you scroll
         gsap.fromTo(
           ".craft-your-tale-component",
           { opacity: 0 },
-          { opacity: 1,
+          {
+            opacity: 1,
             ease: "none",
             scrollTrigger: {
               trigger: ".craft-your-tale-component",
@@ -81,12 +97,13 @@ const CraftYourTaleComponent = defineComponent({
             }
           }
         );
-  
+
         // Additional animation: Slight zoom effect on the background container
         gsap.fromTo(
           ".craft-your-tale-component > div",
           { scale: 1.1 },
-          { scale: 1,
+          {
+            scale: 1,
             ease: "none",
             scrollTrigger: {
               trigger: ".craft-your-tale-component",
@@ -98,7 +115,7 @@ const CraftYourTaleComponent = defineComponent({
         );
       });
     });
-  
-    return {  language };
+
+    return { language };
   }
 });

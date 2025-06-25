28b7b7d9f82d26d405c0870cef63f44059f9e2df
fast <tanawat.wongsawan@vml.com>
2025-06-25 19:30:09 +0700

    fix

--- page/smyth/smyth-content-page/kaset-nawamin/component/form-register/component.js
+++ page/smyth/smyth-content-page/kaset-nawamin/component/form-register/component.js
@@ -290,7 +290,7 @@ const FormRegisterComponent = defineComponent({
 
                     // Add the token to the form object
                     object.token = token;
-                    // await axios.post('https://residential2.singhaestate.co.th/privateestate/smyths/droplead.php', object);
+                    await axios.post('https://residential2.singhaestate.co.th/privateestate/smyths/droplead.php', object);
 
 
                     // ensure hidden iframe exists

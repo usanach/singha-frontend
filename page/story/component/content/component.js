783228b149f9f03455313535e9ab0e5ad03f31e7
fast <tanawat.wongsawan@vml.com>
2025-07-02 09:20:39 +0700

    fix story

--- page/story/component/content/component.js
+++ page/story/component/content/component.js
@@ -65,7 +65,7 @@ const ContentComponent = defineComponent({
       const total = articles.value.length;
       const shown = Math.min(visibleCount.value * 3, total);
       const label = language.value === 'en' ? 'contents' : 'เรื่องน่าสนใจ';
-      return `${total} ${label} (${shown}/${total})`;
+      // return `${total} ${label} (${shown}/${total})`;
     });
 
     onMounted(async () => {
@@ -87,7 +87,7 @@ const ContentComponent = defineComponent({
   template: `
     <section class="content-trigger-pin">
       <div class="relative bg-[url('./../assets/image/story/story-bg.svg')] bg-cover bg-no-repeat bg-bottom">
-        <div class="container py-10 lg:pt-20">
+        <div class="container py-10">
           <div class="pagination lg:w-3/4 mx-auto">
             <h2 class="lg:text-[20px] text-[14px] font-normal text-white uppercase" data-aos="fade-in" data-aos-duration="1000" data-aos-easing="linear">
               {{ headingText }}
@@ -102,16 +102,16 @@ const ContentComponent = defineComponent({
                     <!-- Large Item -->
                     <div class="lg:w-1/2 w-full flex flex-col gap-5" v-if="page.items[0]">
                       <img :src="page.items[0].thumb" :alt="page.items[0].topic" data-aos="fade-up" data-aos-duration="700" data-aos-easing="linear" data-aos-anchor=".content-trigger-pin">
-                      <p class="uppercase text-[14px] border border-[3px] border-[#786028] border-r-0 border-t-0 border-b-0 leading-tight pl-3 text-white" data-aos="fade-up">
+                      <p class="uppercase text-[15px] border border-[3px] border-[#786028] border-r-0 border-t-0 border-b-0 leading-tight pl-3 text-white" data-aos="fade-up">
                         {{ page.items[0].cate }}
                       </p>
-                      <h3 @click="selectArticle(page.items[0].url[language], page.items[0].topic)" class="text-white font-normal text-[24px] leading-snug cursor-pointer" data-aos="fade-up">
+                      <h3 @click="selectArticle(page.items[0].url[language], page.items[0].topic)" class="text-white font-normal text-[22px] leading-snug cursor-pointer" data-aos="fade-up">
                         {{ page.items[0].title }}
                       </h3>
                       <p class="text-white text-[16px]" data-aos="fade-up">
                         {{ page.items[0].description.slice(0,100) }}...
                       </p>
-                      <p class="text-[#A3A3A3] text-[12px]" data-aos="fade-up">
+                      <p class="text-[#A3A3A3] text-[15px]" data-aos="fade-up">
                         {{ page.items[0].date }}
                       </p>
                       <hr class="border border-t-0 border-l-0 border-r-0 border-white/30" />
@@ -122,16 +122,16 @@ const ContentComponent = defineComponent({
                         <div class="flex gap-5 lg:gap-0 relative">
                           <img class="w-2/5 md:h-[180px] h-[150px] object-cover" :src="item.thumb" :alt="item.topic" data-aos="fade-in" data-aos-duration="1000" data-aos-easing="linear" data-aos-anchor=".content-trigger-pin">
                           <div class="w-3/5 lg:px-5 lg:pb-2 flex flex-col gap-2 h-full">
-                            <p class="uppercase text-[14px] border border-[3px] border-[#786028] border-r-0 border-t-0 border-b-0 leading-tight pl-3 text-white" data-aos="fade-up">
+                            <p class="uppercase text-[15px] border border-[3px] border-[#786028] border-r-0 border-t-0 border-b-0 leading-tight pl-3 text-white" data-aos="fade-up">
                               {{ item.cate }}
                             </p>
                             <h3 @click="selectArticle(item.url[language], item.topic)" class="text-white font-normal lg:text-[22px] text-[18px] leading-snug cursor-pointer" data-aos="fade-up">
                               {{ item.title }}
                             </h3>
-                            <p class="text-white lg:text-[16px] text-[14px]" data-aos="fade-up">
+                            <p class="text-white text-[16px]" data-aos="fade-up">
                               {{ item.description.slice(0,100) }}...
                             </p>
-                            <p class="text-[#A3A3A3] text-[12px]" data-aos="fade-up">
+                            <p class="text-[#A3A3A3] text-[15px]" data-aos="fade-up">
                               {{ item.date }}
                             </p>
                           </div>
@@ -146,7 +146,7 @@ const ContentComponent = defineComponent({
           </div>
 
           <div class="flex">
-            <button v-if="visibleCount < totalPages" @click="expandMore" class="btn mt-10 mx-auto" data-aos="fade-up" data-aos-duration="500">
+            <button v-if="visibleCount < totalPages" @click="expandMore" class="btn mt-10 mx-auto font-['SinghaEstate']" data-aos="fade-up" data-aos-duration="500">
               {{ language === 'en' ? 'Explore more' : 'ดูเพิ่มเติม​' }}
             </button>
           </div>

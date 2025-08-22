34181a6613c90cbcf5632389e654046c25ec522e
fast <tanawat.wongsawan@vml.com>
2025-08-22 11:13:58 +0700

    fix

--- page/sentre/sentre-pattanakarn-page/component/gallery/component.js
+++ page/sentre/sentre-pattanakarn-page/component/gallery/component.js
@@ -9,12 +9,12 @@ const GalleryComponent = defineComponent({
                    {{title[language]}}
                 </h2>
                 <!-- Category Buttons -->
-                <div class="gallery-controls flex gap-4 mb-6 justify-center" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="linear">
+                <div class="gallery-controls flex gap-4 mb-6 lg:justify-center lg:px-0 px-3 lg:overflow-x-auto overflow-x-scroll" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="linear">
                     <button
                         v-for="cat in categories"
                         :key="cat.cate"
                         :data-gallery="cat.cate"
-                        class="py-2 text-white text-[20px]"
+                        class="py-2 text-white text-[20px] min-w-fit"
                         :class="{ 'font-bold': activeGallery === cat.cate }"
                         :style="{color:'#5A5B3F'}"
                         @click="handleButtonClick(cat.cate)"
@@ -22,6 +22,7 @@ const GalleryComponent = defineComponent({
                         {{ cat.title[language] }}
                     </button>
                 </div>
+
 
                 <!-- Desktop Carousel -->
                 <div v-if="desktopSlides.length" class="gallery-content lg:block hidden" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="linear">

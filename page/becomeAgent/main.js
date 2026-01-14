b4af969cd7bc15e7c815921141ae4c48f94834e1
fast <tanawat.wongsawan@vml.com>
2026-01-14 10:28:37 +0700

    แก้ไข privacy

--- page/becomeAgent/main.js
+++ page/becomeAgent/main.js
@@ -179,8 +179,8 @@ createApp({
 
           this.form_section.input_text.terms =
             lang === 'en'
-              ? "I agree to receive more information about products, services, and marketing news ... <a class='notice-bold' href='https://www.singhaestate.co.th/en/privacy-notice' target='_blank'>Privacy Notice.​</a>"
-              : "ท่านตกลงรับข้อมูลเกี่ยวกับผลิตภัณฑ์... <a class='notice-bold' href='https://www.singhaestate.co.th/th/privacy-notice' target='_blank'>นโยบายความเป็นส่วนตัว</a>​";
+              ? "I agree to receive more information about products, services, and marketing news of Singha Estate Group of Companies and our business partner, and acknowledge the terms and purposes of data usage in the <a class='notice-bold' href='https://www.singhaestate.co.th/en/privacy-notice' target='_blank'>Privacy Notice.​</a>"
+              : "ท่านตกลงรับข้อมูลเกี่ยวกับผลิตภัณฑ์, บริการ และข่าวสารกิจกรรมของกลุ่มธุรกิจบริษัทในเครือสิงห์ เอสเตท และพันธมิตรของบริษัทฯ และรับทราบข้อกำหนด และวัตถุประสงค์การใช้ข้อมูลที่ระบุไว้ใน <a class='notice-bold' href='https://www.singhaestate.co.th/th/privacy-notice' target='_blank'>นโยบายความเป็นส่วนตัว</a>​";
         }
 
         this.opportunity_section.card_list = section2.map((it, idx) => {

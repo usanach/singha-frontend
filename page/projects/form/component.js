48b32e12e087b16cc48e4e5b1f802165b5d77b51
fast <tanawat.wongsawan@vml.com>
2026-01-15 16:18:31 +0700

    เพิ่ม projectId , lang

--- page/projects/form/component.js
+++ page/projects/form/component.js
@@ -328,8 +328,12 @@ const FormRegisterComponent = defineComponent({
                     const token = await grecaptcha.execute('6LevUS0nAAAAAInOUaytl6bgNgWFE4FQt2yofWyZ', { action: 'submit' });
                     object.token = token;
 
-                    console.log(object)
-                    await axios.post(`https://residential-uat.singhaestate.co.th/leadadmin/api/droplead-project`, object);
+                    let objectDroplead  = {
+                        ...object,
+                        projectId: projectIDs,
+                        lang:language.value
+                    }
+                    await axios.post(`https://residential-uat.singhaestate.co.th/leadadmin/api/droplead-project`, objectDroplead);
                     
                     // ensure hidden iframe exists
                     let iframe = document.getElementById('zapier-iframe');
@@ -441,12 +445,11 @@ const FormRegisterComponent = defineComponent({
                 }
 
                 const res = await getProjectBudget(projectId.value); // ✅ ใช้ api.js
-                const rows = Array.isArray(res?.data) ? res.data : []; // ถ้า api.js คืน {data:{...}} ให้ปรับบรรทัดนี้ตามจริง
+                const rows = Array.isArray(res?.data?.data) ? res.data.data : []; // ถ้า api.js คืน {data:{...}} ให้ปรับบรรทัดนี้ตามจริง
 
                 // filter budget_disabled != 1
                 const enabledRows = rows.filter(row => (row.budget_disabled ?? 0) != 1);
 
-                console.log(res);
                 budgets.value = enabledRows.map(row => ({
                     id: row.id,
                     title: {

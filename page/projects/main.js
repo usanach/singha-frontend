85a30bd80a09cdfeeb5cb4f0eace3de6a39d9b68
fast <tanawat.wongsawan@vml.com>
2026-02-05 21:53:06 +0700

    test

--- page/projects/main.js
+++ page/projects/main.js
@@ -1,22 +1,32 @@
+// ตรวจ URL ปัจจุบัน
+const isExtroPage = window.location.pathname === '/condominium/the-extro/phayathai-rangnam';
+
+// เลือก component ที่จะใช้
+const CraftYourTale = isExtroPage
+  ? CraftYourTaleExtroComponent
+  : CraftYourTaleComponent;
 
 // Create and mount the Vue app
 createApp({
-    components: {
-        HeaderComponent,
-        SubHeaderComponent,
-        BannerComponent,
-        CraftYourTaleComponent,
-        FormRegisterComponent,
-        DesignConceptComponent,
-        ProjectsHighlightComponent,
-        ProjectInformationComponent,
-        ShowCaseComponent,
-        GalleryComponent,
-        LocationComponent,
-        LifeStyleComponent,
-        RelatedProjectsComponent,
-        MoreInfoComponent,
-        FooterComponent,
-        VdoComponent
-    }
+  components: {
+    HeaderComponent,
+    SubHeaderComponent,
+    BannerComponent,
+
+    // 👇 ใช้ตัวที่เลือกไว้
+    CraftYourTaleComponent: CraftYourTale,
+
+    FormRegisterComponent,
+    DesignConceptComponent,
+    ProjectsHighlightComponent,
+    ProjectInformationComponent,
+    ShowCaseComponent,
+    GalleryComponent,
+    LocationComponent,
+    LifeStyleComponent,
+    RelatedProjectsComponent,
+    MoreInfoComponent,
+    FooterComponent,
+    VdoComponent
+  }
 }).mount('#app');

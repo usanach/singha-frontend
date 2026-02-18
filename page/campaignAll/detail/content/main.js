fe4f9bc3280c71e0e754fa42064162f29754ad82
fast <tanawat.wongsawan@vml.com>
2026-02-18 22:27:15 +0700

    test

--- page/campaignAll/detail/content/main.js
+++ page/campaignAll/detail/content/main.js
@@ -199,14 +199,17 @@ const ContentComponent = defineComponent({
             return match ? match[1] : 'th'; // Default to 'th' if not found
         };
         const cleanExtraBreaks = (html = '') => {
-        return html
-            // ถ้ามี <p><br></p> 2 ตัวขึ้นไป ให้เหลือ 1
-            .replace(/(<p>\s*<br\s*\/?>\s*<\/p>\s*){2,}/gi, '<p><br></p>')
-            // ถ้ามี <p><br></p> แค่ 1 ตัว ให้ลบทิ้ง
-            .replace(/^\s*<p>\s*<br\s*\/?>\s*<\/p>\s*$/gi, '')
-            // จำกัด <br> ซ้ำให้เหลือ 1
-            .replace(/(<br\s*\/?>\s*){2,}/gi, '<br>');
+            return html
+                // ลบ <p><br></p> ที่อยู่หลัง h1-h6
+                .replace(/(<h[1-6][^>]*>.*?<\/h[1-6]>)\s*(<p>\s*<br\s*\/?>\s*<\/p>\s*)+/gi, '$1')
+                
+                // จำกัด <p><br></p> ซ้ำให้เหลือ 1
+                .replace(/(<p>\s*<br\s*\/?>\s*<\/p>\s*){2,}/gi, '<p><br></p>')
+
+                // จำกัด <br> ซ้ำให้เหลือ 1
+                .replace(/(<br\s*\/?>\s*){2,}/gi, '<br>');
         };
+
 
         window.addEventListener('resize', () => {
             const rawDetail = matched[`data_detail_${lang}`] || '';

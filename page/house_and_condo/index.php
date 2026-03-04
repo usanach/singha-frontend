071fc50e4ff2e3dbcd4ce85115cb71b427a4b5c0
fast <tanawat.wongsawan@vml.com>
2026-03-04 10:27:30 +0700

    add new ga

--- page/house_and_condo/index.php
+++ page/house_and_condo/index.php
@@ -232,6 +232,14 @@ if (isset($SEO_CONFIG[$pageType])) {
     </script>
     <!-- Google tag (gtag.js) -->
 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7YTP9VBFXQ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-7YTP9VBFXQ');
+    </script>
     <script defer>
         <?php 
         if($pageType == "condo"){ ?>
--- page/story/detail/index.php
+++ page/story/detail/index.php
@@ -342,6 +342,14 @@ if ($articleJson !== false) {
 
         gtag('config', 'G-MNKFVS8Q98');
     </script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7YTP9VBFXQ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-7YTP9VBFXQ');
+    </script>
     <!-- Google tag (gtag.js) -->
 </body>
 

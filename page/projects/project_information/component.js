3ddd3e53033954050e6c0be7f1611cf6a097555a
fast <tanawat.wongsawan@vml.com>
2026-03-18 00:10:35 +0700

    test

--- page/projects/project_information/component.js
+++ page/projects/project_information/component.js
@@ -416,11 +416,7 @@ const ProjectInformationComponent = defineComponent({
           : row.project_pdf;
 
       if (pdfFile) {
-        brochureFilename.value =
-          /^https?:\/\//i.test(pdfFile)
-            ? pdfFile
-            : `${STORAGE_BASE}uploads/documents/${pdfFile}`;
-            
+        brochureFilename.value = pdfFile;
         const STORAGE_BASE = window.APP_CONFIG?.storageUrl || null;
         // ถ้า API ส่งมาเป็นชื่อไฟล์
         brochureUrl.value =
